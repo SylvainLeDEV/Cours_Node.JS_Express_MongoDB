ef793eae6aec76405bcaeb50dce269049259535b
Sylvain <dev.tess.sylvain@gmail.com>
2021-11-24 12:42:39 +0100

    Mise en place de mongoDB avec le models Thing

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Thing = require('./models/Thing');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://go-fullstack-fr:1234@cluster0.akwyw.mongodb.net/est?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 // Exemple de Miffdelware //Middelware 1 test
 // app.use((req, res,next) => {
 //     console.log('Requete reçue !');
@@ -27,39 +40,32 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+app.use(bodyParser.json());
+
 // Requête POST : Middelware qui traite les requête POST
 app.post('/api/stuff', (req, res, next) => {
+    // console.log(req.body);
+    // res.status(201).json({
+    //     message: 'Object crée'
+    // })
+
+    delete req.body._id;
+    const thing = new Thing({
+        // L'opérateur spread = La syntaxe de décomposition permet de développer une expression là où plusieurs arguments (dans le cas des appels à une fonction) ou là où plusieurs éléments (dans le cas des tableaux) sont attendus.
+        ...req.body
+    });
+// fonction save enregistre les données dans la base de données
+    thing.save()
+        .then(() => {res.status(201).json({message : "Objet enregisté ! "})})
+        .catch(error => res.status(400).json({ error }));
 
 })
 
 app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihize',
-            title: 'Mon troisième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-    ];
-    res.status(200).json(stuff);
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json(error));
 })
 
 module.exports = app;
